b69be397c6da46664d2afe27841a1478349b3ebf
Priyansh Soni <87078849+Priyanshsoniii@users.noreply.github.com>
2025-05-26 16:29:51 +0530

    Delete CoordinatesFinder directory

--- CoordinatesFinder/IUrlEncoder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class IUrlEncoder {
-public:
-    virtual ~IUrlEncoder() = default;
-    virtual std::string encode(const std::string& input) const = 0;
-};
--- CoordinatesFinder/include/IJsonParser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <nlohmann/json.hpp>
-
-class IJsonParser {
-public:
-    virtual ~IJsonParser() = default;
-    virtual nlohmann::json parse(const std::string& jsonString) const = 0;
-};
